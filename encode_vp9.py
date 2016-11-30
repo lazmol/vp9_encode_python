074c1308aedafe8edbc77bd68d4c05be2ad7cf91
Laszlo Molnar <lazlowmiller@gmail.com>
2016-11-30 08:14:29 +0100

    Some cleanup

--- encode_vp9.py
+++ encode_vp9.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import os
+import glob
 import subprocess as sp
 import shlex
 import time
@@ -12,7 +13,7 @@ logging.basicConfig(filename='encodings.log', level=logging.INFO)
 def find_files_based_on_extension_within_directory(directory, extension):
     '''Return a list containing files with the provided extension'''
 
-    return [f for f in os.listdir(directory) if f.endswith(extension)]
+    return [os.path.join(directory, f) for f in os.listdir(directory) if f.endswith(extension)]
 
 
 def find_files_based_on_extension(directory, extension):
@@ -70,7 +71,7 @@ def vp9_encode_2pass(inp_file, out_file=None, crf=33, num_cpu=4,
 
     os.system('rm -fr *pass*.log')
 
-    if out_file is None:
+    if not out_file:
         inp_path, inp_name = os.path.split(inp_file)
         inp_name = os.path.splitext(inp_name)[0]
         if nofilter:
@@ -80,7 +81,7 @@ def vp9_encode_2pass(inp_file, out_file=None, crf=33, num_cpu=4,
                        inp=inp_name, crf=crf, hqdn=hqdn, grad=gradfun, sharp=unsharp)
         out_file = os.path.join(inp_path, out_name)
 
-    print(inp_file, out_file)
+    print('Encoding:', inp_file, out_file)
     speed = str(speed)
     num_cpu = str(num_cpu)
     if isinstance(crf, str):
@@ -88,14 +89,14 @@ def vp9_encode_2pass(inp_file, out_file=None, crf=33, num_cpu=4,
     else:
         crf = str(crf)
 
-    command_p1 = 'ffmpeg -y -i {in_file} -c:v libvpx-vp9 -pass 1 -b:v {br_v} ' \
-                 '-crf {crf} -threads {num_cpu} -speed 4 -tile-columns 6 -frame-parallel 1 ' \
-                 '-an -f webm /dev/null'
-    command_p2 = 'ffmpeg -i {in_file} -c:v libvpx-vp9 -pass 2 -g 25 -b:v {br_v} ' \
-                 '-crf {crf} -vf "yadif, hqdn3d={hqdn}, gradfun={grad}, unsharp={unsharp}" ' \
-                 '-threads {num_cpu} -speed {speed} -tile-columns 6 -frame-parallel 1 ' \
-                 '-auto-alt-ref 1 -lag-in-frames 25 -c:a libopus -b:a {br_a} ' \
-                 '-f webm {out_file}'
+    command_p1 = ('ffmpeg -y -i {in_file} -c:v libvpx-vp9 -pass 1 -b:v {br_v} '
+                  '-crf {crf} -threads {num_cpu} -speed 4 -tile-columns 6 -frame-parallel 1 '
+                  '-an -f webm /dev/null')
+    command_p2 = ('ffmpeg -i {in_file} -c:v libvpx-vp9 -pass 2 -g 25 -b:v {br_v} '
+                  '-crf {crf} -vf "yadif, hqdn3d={hqdn}, gradfun={grad}, unsharp={unsharp}" '
+                  '-threads {num_cpu} -speed {speed} -tile-columns 6 -frame-parallel 1 '
+                  '-auto-alt-ref 1 -lag-in-frames 25 -c:a libopus -b:a {br_a} '
+                  '-f webm {out_file}')
     command_p1 = command_p1.format(in_file=inp_file, br_v=br_v, crf=crf, num_cpu=num_cpu)
     command_p2 = command_p2.format(in_file=inp_file, br_v=br_v, crf=crf, num_cpu=num_cpu,
                                    speed=speed, br_a=br_a, out_file=out_file,
@@ -110,7 +111,6 @@ def vp9_encode_2pass(inp_file, out_file=None, crf=33, num_cpu=4,
     if unsharp == '0:0:0.0:0:0:0.0':
         command_p2 = command_p2.replace(', unsharp=' + unsharp, '')
 
-
     args1 = shlex.split(command_p1)
     args2 = shlex.split(command_p2)
     if nofilter:
@@ -124,22 +124,26 @@ def vp9_encode_2pass(inp_file, out_file=None, crf=33, num_cpu=4,
     process.wait()
 
 
-if __name__ == '__main__':
-    avi_files = find_files_based_on_extension_within_directory('./', 'avi')
-    print(avi_files)
-    for avi_file in avi_files:
-        out_file = os.path.splitext(avi_file)[0] + '.webm'
-        vp9_encode_2pass(avi_file, out_file=out_file, crf=31, num_cpu=6, br_v='1000K', br_a='96k', speed=1,
-                         hqdn='luma_spatial=2', gradfun='1.0:16', unsharp='9:9:1.0:3:3:0.0')
-    stop
-    # strength: .51 to 64,  radius: 8-32
-    gradfuns = ['0.51:32', '1.0:16']
-    hqdns = ['luma_spatial=2']
-    # luma 3-63 ODD!! but more than 11 is bad!! ,luma size no more than 25 chroma close to zero
-    unsharps = ['9:9:1.0:3:3:0.0', '7:7:1.0:3:3:0.0']
-    # final best ssettings are 'luma_spatial=2', '1.0:16', '9:9:1.0:3:3:0.0'
-
+def test_filters(input_file,
+                 gradfuns=['0.51:32', '1.0:16'],
+                 hqdns=['luma_spatial=2'],
+                 unsharps=['9:9:1.0:3:3:0.0', '7:7:1.0:3:3:0.0']):
+    '''Method to try different filter parameters
+    gradfun: strength: .51 to 64,  radius: 8-32
+    unsharp: luma 3-63 ODD!! but more than 11 is bad!! ,luma size no more than 25 chroma close to zero
+    final best ssettings are 'luma_spatial=2', '1.0:16', '9:9:1.0:3:3:0.0'
+    '''
     for gradfun, hqdn, unsharp in it.product(gradfuns, hqdns, unsharps):
         # logging.info(('Current filters:', gradfun, hqdn, unsharp))
-        vp9_encode_2pass('Horv.avi', crf=31, num_cpu=3, br_v='1000K', br_a='96k', speed=1,
+        vp9_encode_2pass(input_file, crf=31, num_cpu=3, br_v='1000K', br_a='96k', speed=1,
                          hqdn=hqdn, gradfun=gradfun, unsharp=unsharp)
+
+
+if __name__ == '__main__':
+    inp_files = sorted(glob.glob('./*.avi'))
+    print(inp_files)
+    for inp_file in inp_files:
+        out_file = os.path.splitext(inp_file)[0] + '.webm'
+        vp9_encode_2pass(inp_file, out_file=out_file,
+                         crf=31, num_cpu=7, br_v='1000K', br_a='96k', speed=1,
+                         hqdn='luma_spatial=2', gradfun='1.0:16', unsharp='9:9:1.0:3:3:0.0')
